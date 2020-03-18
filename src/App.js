f90073cd9f3dd1d26b85a41618b4c72cdb0a1d49
syj1198 <seoyeonju1198@gmail.com>
2020-03-18 22:32:51 +0900

    to Create-form

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, {Component} from 'react';
 import './App.css';
 import TOC from "./components/TOC";
 import Subject from "./components/Subject";
-import Content from "./components/Content";
+import ReadContent from "./components/ReadContent";
+import CreateContent from "./components/CreateContent";
+import Control from "./components/Control";
 import TestDP from "./components/TestDP";
 
 
@@ -11,21 +13,62 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state ={
+      mode : 'create',
+      selected_content_id:2,
       subject : {title:'WEB', sub:'World Wide Web!'},
+      welcome:{title:'Welcome', desc:"Hello, React!!"},
       contents : [
-        {id:1, title:'HTML', des:'HTML is for information'},
-        {id:2, title:'CSS', des:'CSS is for design'},
-        {id:3, title:'JavaScript', des:'JavaScript is for interactive'}
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
       ]
     }
   }
   render(){
+    console.log("App render");
+    var _title, _desc, _article = null;
+    if(this.state.mode==='welcome'){
+      _title=this.state.welcome.title;
+      _desc=this.state.welcome.desc;
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode==='read'){
+      var i=0;
+      while(i<this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title=data.title;
+          _desc=data.desc;
+          break;
+        }
+        i=i+1;
+      }
+      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode ==='create'){
+      _article = <CreateContent></CreateContent>
+    }
+
     return (
       <div className="App">
         <Subject title={this.state.subject.title} 
-        sub={this.state.subject.sub} ></Subject>
-        <TOC data={this.state.contents}></TOC>
-        <Content></Content>
+        sub={this.state.subject.sub}
+        onChangePage={function(){
+          this.setState({mode:'welcome'});
+        }.bind(this)}
+        ></Subject>
+        <TOC data={this.state.contents}
+          onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selected_content_id:Number(id)
+          });
+        }.bind(this)}
+        ></TOC>
+        <Control onChangeMode={function(_mode){
+          this.setState({
+            mode:_mode
+          });
+        }.bind(this)}></Control>
+        {_article}
         <TestDP></TestDP>
       </div>
     );
