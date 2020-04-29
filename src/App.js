c0ffa67438f38774ba05c1e80e246375678a3df1
syj1198 <seoyeonju1198@gmail.com>
2020-04-29 22:35:22 +0900

    to Delete Content : done...git status

--- src/App.js
+++ src/App.js
@@ -98,9 +98,30 @@ class App extends Component{
         }.bind(this)}
         ></TOC>
         <Control onChangeMode={function(_mode){
-          this.setState({
-            mode:_mode
-          });
+          if(_mode==='delete'){
+            if(window.confirm('really?')){
+              var _contents = Array.from(this.state.contents);
+              var i=0;
+              while(i<_contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i,i+1);
+                  break;
+                }
+                i=i+1;
+              }
+              this.setState({
+                mode:'welcome',
+                contents:_contents
+              })
+            }
+
+          }
+          else{
+            this.setState({
+              mode:_mode
+            });
+          }
+          
         }.bind(this)}></Control>
         {this.getContent()}
         <TestDP></TestDP>
